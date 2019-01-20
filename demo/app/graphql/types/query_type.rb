ca91baa4247b5bdfbaea9842b48dfc6c22ebf4c2
Yuki AKAMATSU <yuki-akamatsu@cookpad.com>
2019-01-20 18:25:22 +0900

    Use connection_type

--- demo/app/graphql/types/query_type.rb
+++ demo/app/graphql/types/query_type.rb
@@ -1,7 +1,7 @@
 module Types
   class QueryType < Types::BaseObject
-    field :users, [UserType], "returns all users", null: false
-    field :posts, [PostType], "returns all posts", null: false
+    # field :users, [UserType], "returns all users", null: false
+    field :users, UserType.connection_type, "returns all users", null: false
 
     def users
       User.all
