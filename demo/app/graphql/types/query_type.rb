4fa525bce41db102665fba795edd91fb5141ffb7
Yuki AKAMATSU <yuki-akamatsu@cookpad.com>
2019-01-20 18:20:56 +0900

    Define resolvers

--- demo/app/graphql/types/query_type.rb
+++ demo/app/graphql/types/query_type.rb
@@ -2,5 +2,9 @@ module Types
   class QueryType < Types::BaseObject
     field :users, [UserType], "returns all users", null: false
     field :posts, [PostType], "returns all posts", null: false
+
+    def users
+      User.all
+    end
   end
 end
--- demo/app/graphql/types/user_type.rb
+++ demo/app/graphql/types/user_type.rb
@@ -5,5 +5,17 @@ module Types
     field :name, String, "a user's name", null: true
     field :age, Integer, "a user's age", null: true
     field :posts, [Types::PostType], "a user's posts", null: true
+
+    def name
+      object.name
+    end
+
+    def age
+      object.age
+    end
+
+    def posts
+      object.posts
+    end
   end
 end
