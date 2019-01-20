13d02ca13865bdbf92bb0b4fc35d054298ca8c35
Yuki AKAMATSU <yuki-akamatsu@cookpad.com>
2019-01-20 20:29:22 +0900

    Add posts

---
+++ demo/db/migrate/20190120112835_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.belongs_to :user
+
+      t.timestamps
+    end
+
+    add_foreign_key :posts, :users
+  end
+end
--- demo/db/schema.rb
+++ demo/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_085129) do
+ActiveRecord::Schema.define(version: 2019_01_20_112835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2019_01_20_085129) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
